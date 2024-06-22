47e061cc299361fb7ae7f30a41e5fa582abf4734
87690ut <118200430+87690ut@users.noreply.github.com>
2024-06-23 01:38:11 +0530

    Add files via upload

---
+++ first.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    printf("uttam tiwari \n hi user");  
+    int x;
+    x = 5;
+    printf("\n");
+    printf("%d",x);
+    x = x + 5;
+    printf("\n");
+    printf("%d",x);
+x = x - 2;
+printf("\n");
+printf("%d",x);
+printf("\n");
+    return 0; 
+}
